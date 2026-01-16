030acd59044187c4ffcfebdf30eb8f5f50b75147
App Generator <51070104+app-generator@users.noreply.github.com>
2026-01-16 12:05:47 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    field = models.IntegerField(null=True, blank=True)
+    campo = models.TextField(max_length=255, null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Campos(models.Model):
+
+    #__Campos_FIELDS__
+    field1 = models.BooleanField()
+    field2f = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    field3 = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Campos_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Campos")
+        verbose_name_plural = _("Campos")
+
+
+
+#__MODELS__END
